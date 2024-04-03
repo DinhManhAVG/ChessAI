63d68f25b3495e13b926bb3a15f431c09fae9f5a
Dinh Manh <tranmanhproh1@gmail.com>
2024-04-03 17:28:04 +0700

    feat: [AI] Alpha beta pruning and improve AI

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -8,9 +8,65 @@ piece_score = {
     "N": 3,
     "p": 1,
 }
+# Mảng này dùng để đánh giá vị trí của quân cờ trên bàn cờ để ưu tiên quân cờ đi vào vị trí có điểm cao hơn
+knight_scores = [[0.0, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.0],
+                 [0.1, 0.3, 0.5, 0.5, 0.5, 0.5, 0.3, 0.1],
+                 [0.2, 0.5, 0.6, 0.65, 0.65, 0.6, 0.5, 0.2],
+                 [0.2, 0.55, 0.65, 0.7, 0.7, 0.65, 0.55, 0.2],
+                 [0.2, 0.5, 0.65, 0.7, 0.7, 0.65, 0.5, 0.2],
+                 [0.2, 0.55, 0.6, 0.65, 0.65, 0.6, 0.55, 0.2],
+                 [0.1, 0.3, 0.5, 0.55, 0.55, 0.5, 0.3, 0.1],
+                 [0.0, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.0]]
+
+bishop_scores = [[0.0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.0],
+                 [0.2, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.2],
+                 [0.2, 0.4, 0.5, 0.6, 0.6, 0.5, 0.4, 0.2],
+                 [0.2, 0.5, 0.5, 0.6, 0.6, 0.5, 0.5, 0.2],
+                 [0.2, 0.4, 0.6, 0.6, 0.6, 0.6, 0.4, 0.2],
+                 [0.2, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.2],
+                 [0.2, 0.5, 0.4, 0.4, 0.4, 0.4, 0.5, 0.2],
+                 [0.0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.0]]
+
+rook_scores = [[0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25],
+               [0.5, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.5],
+               [0.0, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.0],
+               [0.0, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.0],
+               [0.0, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.0],
+               [0.0, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.0],
+               [0.0, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.0],
+               [0.25, 0.25, 0.25, 0.5, 0.5, 0.25, 0.25, 0.25]]
+
+queen_scores = [[0.0, 0.2, 0.2, 0.3, 0.3, 0.2, 0.2, 0.0],
+                [0.2, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.2],
+                [0.2, 0.4, 0.5, 0.5, 0.5, 0.5, 0.4, 0.2],
+                [0.3, 0.4, 0.5, 0.5, 0.5, 0.5, 0.4, 0.3],
+                [0.4, 0.4, 0.5, 0.5, 0.5, 0.5, 0.4, 0.3],
+                [0.2, 0.5, 0.5, 0.5, 0.5, 0.5, 0.4, 0.2],
+                [0.2, 0.4, 0.5, 0.4, 0.4, 0.4, 0.4, 0.2],
+                [0.0, 0.2, 0.2, 0.3, 0.3, 0.2, 0.2, 0.0]]
+
+pawn_scores = [[0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
+               [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7],
+               [0.3, 0.3, 0.4, 0.5, 0.5, 0.4, 0.3, 0.3],
+               [0.25, 0.25, 0.3, 0.45, 0.45, 0.3, 0.25, 0.25],
+               [0.2, 0.2, 0.2, 0.4, 0.4, 0.2, 0.2, 0.2],
+               [0.25, 0.15, 0.1, 0.2, 0.2, 0.1, 0.15, 0.25],
+               [0.25, 0.3, 0.3, 0.0, 0.0, 0.3, 0.3, 0.25],
+               [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]]
+
+piece_position_scores = {"wN": knight_scores,
+                         "bN": knight_scores[::-1],
+                         "wB": bishop_scores,
+                         "bB": bishop_scores[::-1],
+                         "wQ": queen_scores,
+                         "bQ": queen_scores[::-1],
+                         "wR": rook_scores,
+                         "bR": rook_scores[::-1],
+                         "wp": pawn_scores,
+                         "bp": pawn_scores[::-1]}
 CHECKMATE = 1000
 STALEMATE = 0
-DEPTH = 2
+DEPTH = 3
 
 # Random move
 def find_random_move(valid_moves):
@@ -25,12 +81,24 @@ def score_material(board):
     sẽ ưu tiên ăn quân xe vì điểm số của quân xe cao hơn
     """
     score = 0
-    for row in board:
-        for square in row:
-            if square[0] == "w":
-                score += piece_score[square[1]]
-            elif square[0] == "b":
-                score -= piece_score[square[1]]
+    # for row in board:
+    #     for square in row:
+    #         if square[0] == "w":
+    #             score += piece_score[square[1]]
+    #         elif square[0] == "b":
+    #             score -= piece_score[square[1]]
+
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            piece = board[row][col]
+            if piece != "--":
+                piece_position_score = 0
+                if piece[1] != "K":
+                    piece_position_score = piece_position_scores[piece][row][col]
+                if piece[0] == "w":
+                    score += piece_score[piece[1]] + piece_position_score
+                if piece[0] == "b":
+                    score -= piece_score[piece[1]] + piece_position_score
     return score
 
 def score_board(gs):
@@ -55,7 +123,7 @@ def find_best_move(gs, valid_moves):
     random.shuffle(valid_moves)
     for player_move in valid_moves:
         gs.makeMove(player_move)
-        opponent_moves = gs.getValidMoves()
+        opponent_moves = gs.getValidMoves_2()
         if gs.staleMate:
             opponent_max_score = STALEMATE
         elif gs.checkMate:
@@ -65,7 +133,7 @@ def find_best_move(gs, valid_moves):
             for opponent_move in opponent_moves:
                 gs.makeMove(opponent_move)
                 # Sau khi thực hiện nước đi thì gọi hàm này để kiểm tra xem có chiếu tướng hay bế tắc không
-                gs.getValidMoves()
+                gs.getValidMoves_2()
                 if gs.checkMate:
                     score = CHECKMATE
                 elif gs.staleMate:
@@ -79,21 +147,29 @@ def find_best_move(gs, valid_moves):
             opponent_min_max_score = opponent_max_score
             best_player_move = player_move
         gs.undoMove()
-    return best_player_move
+    return  best_player_move
 
-def find_best_move_minimax(gs, valid_moves):
-    """
-    Phương pháp giúp gọi đệ quy lần đầu tiên
-    """
-    print("find_best_move_minimax")
+# def find_best_move_minimax(gs, valid_moves):
+#     """
+#     Phương pháp giúp gọi đệ quy lần đầu tiên
+#     """
+#     print("find_best_move_minimax")
+#     global next_move
+#     next_move = None
+
+#     find_move_minimax(gs, valid_moves, DEPTH, gs.whiteToMove)
+#     return next_move
+
+
+def find_best_move_minimax(gs, valid_moves, returnQueue):
     global next_move
     next_move = None
+    alpha = -CHECKMATE
+    beta = CHECKMATE
+    find_move_minimax(gs, valid_moves, DEPTH, gs.whiteToMove, alpha, beta)
+    returnQueue.put(next_move)
 
-    find_move_minimax(gs, valid_moves, DEPTH, gs.whiteToMove)
-    return next_move
-
-def find_move_minimax(gs, valid_moves, depth, white_to_move):
-    print("find_move_minimax")
+def find_move_minimax(gs, valid_moves, depth, white_to_move, alpha, beta):
     global next_move
     if depth == 0:
         return score_material(gs.board)
@@ -102,23 +178,29 @@ def find_move_minimax(gs, valid_moves, depth, white_to_move):
         max_score = -CHECKMATE
         for move in valid_moves:
             gs.makeMove(move)
-            score = find_move_minimax(gs, gs.getValidMoves(), depth - 1, False)
+            next_moves = gs.getValidMoves_2()
+            score = find_move_minimax(gs, next_moves, depth - 1, False, alpha, beta)
             if score > max_score:
                 max_score = score
                 if depth == DEPTH:
                     next_move = move
             gs.undoMove()
+            alpha = max(alpha, score)
+            if beta <= alpha:
+                break  # beta cut-off
         return max_score
-        
     else:
         min_score = CHECKMATE
         for move in valid_moves:
             gs.makeMove(move)
-            score = find_move_minimax(gs, gs.getValidMoves(), depth - 1, True)
+            next_moves = gs.getValidMoves_2()
+            score = find_move_minimax(gs, next_moves, depth - 1, True, alpha, beta)
             if score < min_score:
                 min_score = score
                 if depth == DEPTH:
                     next_move = move
             gs.undoMove()
+            beta = min(beta, score)
+            if beta <= alpha:
+                break  # alpha cut-off
         return min_score
-    
